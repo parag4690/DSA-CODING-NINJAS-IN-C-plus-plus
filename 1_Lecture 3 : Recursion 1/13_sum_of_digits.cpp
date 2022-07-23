ea3cf1528f321af8ec20400d2b9a58bfbd43da9b
parag sharma <parag0992.be21@chitkara.edu.in>
2022-07-23 12:22:32 +0530

    changing

--- 1_Lecture 3 : Recursion 1/13_sum_of_digits.cpp
+++ 1_Lecture 3 : Recursion 1/13_sum_of_digits.cpp
@@ -1,29 +1,16 @@
-bool checkPalindrome(char input[]) {
+int sumOfDigits(int n) {
     // Write your code here
-   static int c = 0,j;
-  // length of string
-   if(c==0) {
-   for( j = 0; input[j] != '\0'; ++j);
-   }
-    
-   static int n = j-1,i=0;
-   
-   if(n==1){
-       return true;
-   }
-   if(i>=n){
-     return true;
-   }
-   if(input[n]!=input[i]){
-      return false;
-   }
-   else{
-       n=n-1;
-       i=i+1;
-       return checkPalindrome(input);
-   }
 
     
+    static int sum =0;
+    if(n==0){
+       return sum;
+    }
+    else{
+        int rem = n%10;
+        sum+=rem;
+        return sumOfDigits(n/10);
+    }
 }
 
 
